6e973ae448f3c951af6c5d7aeb0c58f6c1b1cb36
Jan Schill <janschill@me.com>
2021-03-30 16:25:58 +0200

    Configure solid-comment in Indico

---
+++ indico/modules/events/client/js/solid.js
@@ -0,0 +1,27 @@
+import solidCommentsURL from 'indico-url:events.solid_comments';
+
+import {indicoAxios} from 'indico/utils/axios';
+import SolidComment from 'solid-comment';
+
+export default async function setupSolidComments(params) {
+  const eventId = params.eventId;
+  const eventProtection = params.eventProtection;
+
+  if (eventId && eventProtection) {
+    const solidCommentId = `sc-development-indico-meeting-${eventProtection}-${eventId}`;
+    const serverUrl = solidCommentsURL({event_id: params.eventId});
+    const solidComment = new SolidComment({
+      solidCommentId,
+      eventVisibility: eventProtection,
+      serverStorageEndpointUrl: serverUrl,
+    });
+
+    await solidComment.initApp(); // render components
+    solidComment.setAppClient(indicoAxios); // use own HTTP client
+    await fetch(serverUrl) // load comments, pass to solid-comment, render
+      .then(response => response.json())
+      .then(data => solidComment.setComments(data));
+  }
+}
+
+window.setupSolidComments = setupSolidComments;
