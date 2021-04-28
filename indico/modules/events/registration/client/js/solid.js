894459c4cae5b216c0206c16098f262f2818494d
Jan Schill <schill@hey.com>
2021-04-28 10:16:52 +0200

    Trigger input event when auto-filling

--- indico/modules/events/registration/client/js/solid.js
+++ indico/modules/events/registration/client/js/solid.js
@@ -4,6 +4,21 @@ document.addEventListener('DOMContentLoaded', async () => {
   observeFormCreation();
 });
 
+function triggerInputEvent(inputs) {
+  for (let i = 0; i < inputs.length; i++) {
+    const element = inputs[i];
+    [element, element.parentNode].forEach(node => {
+      if ('createEvent' in document) {
+        const evt = document.createEvent('HTMLEvents');
+        evt.initEvent('input', false, true);
+        node.dispatchEvent(evt);
+      } else {
+        node.fireEvent('oninput');
+      }
+    });
+  }
+}
+
 function observeFormCreation() {
   const formId = 'registrationForm';
   const $conferencePage = document.querySelector('.conference-page');
@@ -18,7 +33,7 @@ function observeFormCreation() {
             observer.disconnect();
             const solidAutocomplete = new SolidAutocomplete({form: node});
             solidAutocomplete.createAutocompleteDomControls(node);
-            solidAutocomplete.setupSolidAutocomplete();
+            solidAutocomplete.setupSolidAutocomplete(triggerInputEvent);
           }
         }
       }
